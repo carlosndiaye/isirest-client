abc700e389cc6c9e3d8561b5f8fe72e7cbafe53e
Carlos Ndiaye <carlosndiay@gmail.com>
2020-03-01 19:25:37 +0000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePersonComponent } from './create-person/create-person.component';
+import { PersonDetailsComponent } from './person-details/person-details.component';
+import { PersonListComponent } from './person-list/person-list.component';
+import { UpdatePersonComponent } from './update-person/update-person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatePersonComponent,
+    PersonDetailsComponent,
+    PersonListComponent,
+    UpdatePersonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/person.ts.ts
@@ -0,0 +1,8 @@
+export class Person {
+    id: number;
+    firstname: string;
+    lastname: string;
+    address: string;
+    number: number;
+    active: boolean;
+  }
