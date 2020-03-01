64e5ce3581b159f89bd5fb25502a5f7cb29d9af6
Carlos Ndiaye <carlosndiay@gmail.com>
2020-03-01 20:20:05 +0000

    Some changes

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ const path = require('path');
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/isirest-client/index.html'));
 });
